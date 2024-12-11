c9c91b63e0bce8565a4f419ea764731bdf63b13e
drizord <adrianomirandajr@gmail.com>
2024-12-11 14:05:48 -0400

    Update webhook_bot.py

--- webhook_bot.py
+++ webhook_bot.py
@@ -16,4 +16,6 @@ def telegram_webhook():
     return "ok", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
